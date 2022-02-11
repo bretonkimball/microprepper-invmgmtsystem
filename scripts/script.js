e86465943a4d41a14d23780d5e1a6c407e1884ff
Breton K Handy <bretonkhandy@gmail.com>
2022-02-11 14:31:50 -0600

    javascript is hard

--- scripts/script.js
+++ scripts/script.js
@@ -15,36 +15,33 @@ function getShelteringResults() {
     var neededGallonsGas = round(numHours / gPH, 2)
 
         function enoughWaterShelter() {
-            if (totalWater >= neededGallons) {
-                document.write("You have enough water.")
+            if (totalWater >= neededGallonsWater) {
+                document.getElementById("resultsShelteringEnoughWater").innerHTML = "You have enough water.";
             }
             else {
-                document.write("You don't have enough water. You need an additional:", Math.ceil(neededGallonsWater - totalWater),
-                      "gallons, or:", Math.ceil((neededGallonsWater - totalWater) / 0.13203125), "bottles of water.")
+                document.getElementById("resultsShelteringEnoughWater").innerHTML = "You don't have enough water. You need an additional:" +Math.ceil(neededGallonsWater - totalWater) +
+                      "gallons, or:" + Math.ceil((neededGallonsWater - totalWater) / 0.13203125) + "bottles of water.";
             }
         };
 
         function enoughFoodShelter() {
-            document.write("You need a total of:", math.ceil(neededRationsSheltering * 3), "meals.")
-            document.write("This is the equivalent of", math.ceil(neededCaloriesSheltering), "calories, or:")
-            document.write(math.ceil(neededCaloriesSheltering / 6800.0), "bags of Huel meal replacement, or:")
-            document.write(math.ceil((neededRationsSheltering * 3) / 8), "Mountain House #10 Cans.")
+            document.getElementById("resultsShelteringEnoughFood").innerHTML = "You need a total of:" + Math.ceil(neededRationsSheltering * 3) + "meals. This is the equivalent of" + math.ceil(neededCaloriesSheltering) + "calories, or:" + Math.ceil(neededCaloriesSheltering / 6800.0) + "bags of Huel meal replacement, or:" + Math.ceil((neededRationsSheltering * 3) / 8) + "Mountain House #10 Cans.";
         };
 
         function additionalSuppliesShelter() {
-            document.write("You need a total of", math.ceil((numPeopleSheltering * 2) * numWeeks), "N95 masks to reduce the spread of Covid-19.")
-            document.write("You need a total of", math.ceil((numPeopleSheltering * numWeeks) / 2), "bottles of hand sanitizer.")
-            document.write("You need enough prescription medication per person for", numDaysSheltering, "days.")
-            document.write("You need", numPeopleSheltering, "flashlights, and", numPeopleSheltering, "headlamps.")
-            document.write("You need", math.ceil(numPeopleSheltering * numDaysSheltering * 1.5), "protein bars.")
-            document.write("You need", math.ceil((numPeopleSheltering * numDaysSheltering) / 3), "boxes of Cosmic Brownies.")
+            document.getElementById("resultsShelteringN95").innerHTML = "You need a total of" + Math.ceil((numPeopleSheltering * 2) * numWeeks) + "N95 masks to reduce the spread of Covid-19.";
+            document.getElementById("resultsShelteringHandSanitizer").innerHTML = "You need a total of" + Math.ceil((numPeopleSheltering * numWeeks) / 2) + "bottles of hand sanitizer.";
+            document.getElementById("resultsShelteringPrescriptionMeds").innerHTML = "You need enough prescription medication per person for" + numDaysSheltering + "days.";
+            document.getElementById("resultsShelteringLights").innerHTML = "You need" + numPeopleSheltering + "flashlights, and" + numPeopleSheltering + "headlamps.";
+            document.getElementById("resultsShelteringProteinBars").innerHTML = "You need" + Math.ceil(numPeopleSheltering * numDaysSheltering * 1.5) + "protein bars.";
+            document.getElementById("resultsShelteringCosmicBrownies").innerHTML = "You need" + Math.ceil((numPeopleSheltering * numDaysSheltering) / 3) + "boxes of Cosmic Brownies.";
             // This is a New Orleans in-joke.
-            document.write("You need approximately", neededGallonsGas, "gallons of gas for a standard 2000w Honda inverter generator.")
+            document.getElementById("resultsShelteringGas").innerHTML = "You need approximately" + neededGallonsGas + "gallons of gas for a standard 2000w Honda inverter generator.";
         };
 
-        document.getElementById("resultsSheltering").innerHTML = enoughWaterShelter();
-        document.getElementById("resultsSheltering").innerHTML = enoughFoodShelter();
-        document.getElementById("resultsSheltering").innerHTML = additionalSuppliesShelter();
+        enoughWaterShelter();
+        enoughFoodShelter();
+        additionalSuppliesShelter();
 };
 
 //function getEvacuationResults() {
